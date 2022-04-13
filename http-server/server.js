c71d5d70f1120e190e8df95965521a3f2506e37b
Jan Redmann <janredmann1@gmail.com>
2022-04-13 11:38:29 +0200

    add http server

--- http-server/server.js
+++ http-server/server.js
@@ -7,7 +7,7 @@ const server = http.createServer(function (request, response) {
 	if (request.url === '/') {
 		response.end('<h1>this is the home page</h1>')
 	}
-	if (request.url === '/about') {
+	else if (request.url === '/about') {
 		response.end('<h1>this is the about page</h1>')
 	} else {
 		response.end('<h1>404 - document not found</h1>')
